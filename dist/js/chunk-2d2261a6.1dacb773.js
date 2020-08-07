3401c345323438e0b844886102a0569871d72a54
Jung-Yi, Tsai <jungyitsai@gmail.com>
2020-08-07 13:59:55 +0800

    [add] add the dist files

---
+++ dist/js/chunk-2d2261a6.1dacb773.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2261a6"],{e6dc:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("產品列表")])])}],u=t("2877"),l={},a=Object(u["a"])(l,c,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d2261a6.1dacb773.js.map
