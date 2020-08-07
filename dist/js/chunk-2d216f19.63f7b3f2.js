7f262252d5a65b6c3574c1ec8019f18afc53220b
Jung-Yi, Tsai <jungyitsai@gmail.com>
2020-08-07 14:17:15 +0800

    [clean] remove dist files

--- dist/js/chunk-2d216f19.63f7b3f2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d216f19"],{c52c:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("圖片儲存列表")])])}],u=t("2877"),l={},s=Object(u["a"])(l,c,r,!1,null,null,null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d216f19.63f7b3f2.js.map
